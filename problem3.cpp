5ba7e4286dbbfa7b673c6d43c7e79072140287d9
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-12-06 10:25:30 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int fmx(int* a, int ln){
+int findMax(int* a, int ln){
     int mx = *a;
     for (int i=1; i<ln; i++){    if (*(a+i)>mx){    mx = *(a+i);}}
     return mx;
 }
 
-void rvrs(int* a, int ln){
+void reverseArray(int* a, int ln){
     int* bg = a; int* nd = a+ln-1;
     while (bg<nd){
         int tmp = *bg;
@@ -17,7 +17,7 @@ void rvrs(int* a, int ln){
     }
 }
 
-double avg(int* a, int ln){
+double calculateAverage(int* a, int ln){
     int s = 0;    for (int i=0; i<ln; i++){    s+= *(a+i);}
     return static_cast<double>(s)/ln;
 }
@@ -25,11 +25,11 @@ double avg(int* a, int ln){
 int main(){
     int a[100];    int ln = 0, n;
     while (cin >> n && n!=-1){    a[ln++] = n;}
-    int mx = fmx(a, ln);
+    int mx = findMax(a, ln);
     cout << "Maximum value: " << mx << endl;
-    rvrs(a, ln);
+    reverseArray(a, ln);
     cout << "Reversed array: ";
     for (int i = 0; i < ln; ++i){    cout << a[i] << " ";}    
-    cout << fixed << setprecision(1) << "\nAverage: " << avg(a, ln) << endl;
+    cout << fixed << setprecision(1) << "\nAverage: " << calculateAverage(a, ln) << endl;
     return 0;
 }
